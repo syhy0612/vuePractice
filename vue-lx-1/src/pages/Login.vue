c4bbd133466ad1dbb1da59e966365f5933cc9e85
syhy0612 <1843370860@qq.com>
2024-07-13 00:59:24 +0800

    删除无意义注释

--- vue-lx-1/src/pages/Login.vue
+++ vue-lx-1/src/pages/Login.vue
@@ -15,6 +15,10 @@
 
 <script setup>
 import ComponentLoginVue from "@/components/ComponentLogin.vue";
+
+//业务逻辑------------分割线
+console.log("目前暂设默认密码为123456");
+
 </script>
 
 <style lang="scss" scoped>
