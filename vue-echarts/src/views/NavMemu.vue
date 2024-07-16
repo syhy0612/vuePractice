d6a3de1b654a9f79348f34c98be2552292cca5a6
DL若影 <1843370860@qq.com>
2024-07-16 15:30:14 +0800

    wq

--- vue-echarts/src/views/NavMemu.vue
+++ vue-echarts/src/views/NavMemu.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <el-aside width="200px">
-      <el-menu class="el-menu-vertical-demo" 
-      default-active="/about">
+      <el-menu class="el-menu-vertical-demo" default-active="/home" @select="handleSelect">
         <el-menu-item :index="v.index" v-for="v in data" :key="v.index">
-          <el-icon><icon-menu /></el-icon>
+          <el-icon><IconMenu /></el-icon>
           <span>{{ v.name }}</span>
         </el-menu-item>
       </el-menu>
@@ -13,17 +12,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { ElIcon } from 'element-plus'
-import {ref} from 'vue'
+import { Menu as IconMenu } from '@element-plus/icons-vue'
 
-const data =ref([
-  {index:'/home',name:'首页'},
-  {index:'/about',name:'关于'},
-  {index:'/echarts',name:'图表'},
+const router = useRouter()
+
+const data = ref([
+  { index: '/home', name: '首页' },
+  { index: '/about', name: '关于' },
+  { index: '/echarts', name: '图表' }
 ])
 
+const handleSelect = (index) => {
+  router.push(index) // 点击时跳转到对应路由
+}
 </script>
-
 
 <style scoped>
 </style>
