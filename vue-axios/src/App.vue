4c0a8291ab0ca94f71c87babc6ccc36ef0f24360
DL若影 <1843370860@qq.com>
2024-07-23 10:33:29 +0800

    wq

--- vue-axios/src/App.vue
+++ vue-axios/src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div>
     <div class="title">天气预报</div>
-    <input type="text" v-model="latitude">
-    <div>{{ latitude }}</div>
-
+    <div class="inputBox">
+      <span>纬度：</span>
+      <el-input
+          type="text"
+          v-model="latitude"
+          class="inputInfo"
+          placeholder="请输入纬度"
+          :prefix-icon="Search"
+          clearable
+      />
+      <span>{{ latitude }}</span>
+      <div></div>
+      <span>经度：</span>
+      <el-input
+          type="text"
+          v-model="longitude"
+          class="inputInfo"
+          placeholder="请输入经度"
+          :prefix-icon="Search"
+          clearable
+      />
+      <span>{{ longitude }}</span>
+    </div>
   </div>
 </template>
 
 <script setup>
-// import {ref} from "vue";
+import {ref} from "vue";
 import axios from "axios";
+import {Search} from '@element-plus/icons-vue'
 
-const latitude = ref('')
-const longitude = ref('')
+const latitude = ref('29.28')
+const longitude = ref('113.1212')
 
 getTianQi()
 
 async function getTianQi() {
-
   try {
     const res = await axios({
       url: 'https://api.open-meteo.com/v1/forecast?latitude=29.28&longitude=113.1212&daily=weather_code&timezone=auto&forecast_days=14',
@@ -30,12 +50,32 @@ async function getTianQi() {
     console.error('获取天气信息失败:', error);
   }
 }
-
 </script>
 
 <style scoped>
 .title {
   text-align: center;
   font-size: 30px;
+  letter-spacing:10px;
+  color: #13ce66;
+  margin-top: 20px;
+}
+
+.inputInfo {
+  width: 150px;
+  margin-top: 5px;
+  height: 30px;
+}
+
+.inputBox {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 16px;
+}
+.inputBox span {
+  display: inline-block;
+  margin-top: 5px;
+  height: 30px;
+  line-height: 30px;
 }
 </style>
--- vue-axios/vite.config.js
+++ vue-axios/vite.config.js
@@ -1,31 +1,30 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import components from 'unplugin-vue-components/vite';
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-
-import { fileURLToPath, URL } from 'url';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
+import {fileURLToPath, URL} from 'url';
 
 export default defineConfig({
-  server: {
-    port: 8090,
-  },
-  base: './',
-  plugins: [
-    vue(),
-    components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    outDir: 'dist',
-  },
+    server: {
+        port: 8090,
+    },
+    base: './',
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    build: {
+        outDir: 'dist',
+    },
 });
