58e9699644779b21065eff4351b102480e1e2e59
DL若影 <1843370860@qq.com>
2024-07-27 10:25:09 +0800

    左右大盒子安置好  存档wq

--- vue-HIMIT-7.26/src/views/Oqc.vue
+++ vue-HIMIT-7.26/src/views/Oqc.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="container">
+  <div class="mainInfo">
     <!--    左边盒子-->
     <div class="left-box">
       <!--      左上盒子-->
       <div class=" left-box-T">
         <div class="inner-box left-box-T-L">
-
+          a1
         </div>
-        <div class="inner-box left-box-T-R"></div>
+        <div class="inner-box left-box-T-R">b1</div>
       </div>
       <!--      左中盒子-->
       <div class=" left-box-M">
-        <div class="inner-box left-box-M-L"></div>
-        <div class="inner-box left-box-M-R"></div>
+        <div class="inner-box left-box-M-L">c1</div>
+        <div class="inner-box left-box-M-R">d1</div>
       </div>
       <!--      左下盒子-->
       <div class=" left-box-B">
-        <div class="inner-box left-box-B-L"></div>
-        <div class="inner-box left-box-B-R"></div>
+        <div class="inner-box left-box-B-L">e1</div>
+        <div class="inner-box left-box-B-R">f1</div>
       </div>
     </div>
     <!--    右边盒子-->
     <div class="right-box">
       <!--      右上盒子-->
       <div class=" right-box-T">
-        <div class="inner-box right-box-T-L"></div>
-        <div class="inner-box right-box-T-R"></div>
+        <div class="inner-box right-box-T-L">a2</div>
+        <div class="inner-box right-box-T-R">b2</div>
       </div>
       <!--      右中盒子-->
       <div class=" right-box-M">
-        <div class="inner-box right-box-M-L"></div>
-        <div class="inner-box right-box-M-R"></div>
+        <div class="inner-box right-box-M-L">c2</div>
+        <div class="inner-box right-box-M-R">d2</div>
       </div>
       <!--      右下盒子-->
       <div class=" right-box-B">
-        <div class="inner-box right-box-B-L"></div>
-        <div class="inner-box right-box-B-R"></div>
+        <div class="inner-box right-box-B-L">e2</div>
+        <div class="inner-box right-box-B-R">f2</div>
       </div>
     </div>
   </div>
