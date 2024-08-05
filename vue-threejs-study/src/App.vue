4e7cfddf1a3eaf5d8edc3722c6a447bcb1627f3b
syhy0612 <1843370860@qq.com>
2024-08-06 00:45:23 +0800

    wq

--- vue-threejs-study/src/App.vue
+++ vue-threejs-study/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <router-view/>
+
+    <el-container>
+      <el-header>Three.JS</el-header>
+      <el-container>
+        <el-aside>导航菜单哦</el-aside>
+      </el-container>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -9,5 +18,15 @@
 </script>
 
 <style lang="scss" scoped>
+.el-header {
+  text-align: center;
+  font-size: 20px;
+  color: green;
+  padding-top: 20px;
+}
 
+.el-aside {
+  width: 10vw;
+  width: 300px;
+}
 </style>
